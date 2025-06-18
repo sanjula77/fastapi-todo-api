d09aad25ead4a19c830636dbfea67971868d2f7b
Gihan sanjula <sanjulagihan94@gmail.com>
2025-06-19 00:25:54 +0530

    Implement main application logic with CRUD operations for posts

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -10,7 +12,23 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
+try:
+    conn = psycopg2.connect(
+    host='localhost',
+    database='fastApiDb',
+    user='postgres',
+    password='4858@', 
+    cursor_factory=RealDictCursor
+)
+    cursor = conn.cursor()
+    print("Database connection successful")
+
+except Exception as e:
+    print("Database connection failed")
+    print(f"Error: {e}")
+
+
 
 my_posts = [
     {"id": 1, "title": "First Post", "content": "This is the content of the first post."},
@@ -57,3 +75,11 @@ def delete_post(id: int):
     my_posts.pop(index)
     return {"message": "Post deleted successfully"}
 
+@app.put("/posts/{id}")
+def update_post(id: int, payload: Post):
+    index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
+    
+    my_posts[index].update(payload.dict())
+    return {"data": my_posts[index]}
