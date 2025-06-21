1b8ace23f3830031575972102a4e8ed3bf55ac8e
Gihan sanjula <sanjulagihan94@gmail.com>
2025-06-22 01:12:50 +0530

    Update Post model to use server_default for published and created_at fields

--- app/model.py
+++ app/model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -7,4 +8,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="true", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default="now()", nullable=False)
